ee98eb8893d85e879344fac978a5fbb4f526858e
Luan Gustavo <gluan1213@gmail.com>
2025-08-04 20:23:43 -0300

    chore: configura a estrutura inicial do projeto

---
+++ src/test/java/com/ifpr/thread/stilofit/StilofitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpr.thread.stilofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StilofitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
