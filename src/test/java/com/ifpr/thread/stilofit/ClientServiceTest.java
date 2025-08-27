580ac80ad3be2913b6bb303eaa7f5bd9573d2bdc
larybino <binolaryssa@gmail.com>
2025-08-27 19:24:59 -0300

    fix: ajusta o método de teste de exceção quando cpf já existe

--- src/test/java/com/ifpr/thread/stilofit/ClientServiceTest.java
+++ src/test/java/com/ifpr/thread/stilofit/ClientServiceTest.java
@@ -3,6 +3,8 @@ package com.ifpr.thread.stilofit;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.ifpr.thread.stilofit.dto.ClientRequestDTO;
 import com.ifpr.thread.stilofit.exceptions.CpfAlreadyRegisteredException;
 import com.ifpr.thread.stilofit.exceptions.NotBlankException;
+import com.ifpr.thread.stilofit.models.enums.Gender;
 import com.ifpr.thread.stilofit.repositories.ClientRepository;
 import com.ifpr.thread.stilofit.services.ClientService;
 
@@ -26,6 +29,9 @@ class ClientServiceTest {
     @Test
     void create_shouldThrowExceptionWhenCpfAlreadyExists() {
         ClientRequestDTO clientRequestDTO = new ClientRequestDTO();
+        clientRequestDTO.setName("John Doe");
+        clientRequestDTO.setBirthDate(LocalDate.of(2000, 1, 1));
+        clientRequestDTO.setGender(Gender.MASCULINO);
         clientRequestDTO.setCpf("123.456.789-00");
         when(clientRepository.existsByCpf("123.456.789-00")).thenReturn(true);
         assertThrows(CpfAlreadyRegisteredException.class, () -> clientService.create(clientRequestDTO));
@@ -34,9 +40,7 @@ class ClientServiceTest {
     @Test
     void create_shouldThrowExceptionWhenNameIsBlank() {
         ClientRequestDTO clientRequestDTO = new ClientRequestDTO();
-        clientRequestDTO.setName(" "); 
-        clientRequestDTO.setCpf("123.456.789-00");
-
+        clientRequestDTO.setName(" ");
         assertThrows(NotBlankException.class, () -> clientService.create(clientRequestDTO));
     }
 
@@ -46,5 +50,15 @@ class ClientServiceTest {
         clientRequestDTO.setName("John Doe");
         clientRequestDTO.setBirthDate(null);
         assertThrows(NotBlankException.class, () -> clientService.create(clientRequestDTO));
+    }
+
+    @Test
+    void create_shouldThrowExceptionWhenGenderIsNull() {
+        ClientRequestDTO clientRequestDTO = new ClientRequestDTO();
+        clientRequestDTO.setName("John Doe");
+        clientRequestDTO.setBirthDate(LocalDate.of(2000, 1, 1));
+        clientRequestDTO.setGender(null);
+        assertThrows(NotBlankException.class, () -> clientService.create(clientRequestDTO));
+
     }
 }
