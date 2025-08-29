52a4d4974f95b63f6a53a8e9a3dd70fa90d5682b
ThallesSorrilha <thallessorrilha@gmail.com>
2025-08-29 20:07:13 -0300

    fix(ClientServiceTest): corrigindo findById 3

--- src/test/java/com/ifpr/thread/stilofit/ClientServiceTest.java
+++ src/test/java/com/ifpr/thread/stilofit/ClientServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
