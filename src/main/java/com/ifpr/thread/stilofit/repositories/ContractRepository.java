cadd23016fc9bb1aba391f2cd1299470e8b777e5
Vinicius-Oliveira-R <viniciusoliveira12353@gmail.com>
2025-09-26 20:48:12 -0300

    feat(ListarContrato): Atualiza o repositório para receber paginação e ordenação

--- src/main/java/com/ifpr/thread/stilofit/repositories/ContractRepository.java
+++ src/main/java/com/ifpr/thread/stilofit/repositories/ContractRepository.java
@@ -1,8 +1,12 @@
 package com.ifpr.thread.stilofit.repositories;
 
 import com.ifpr.thread.stilofit.models.Contract;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ContractRepository extends JpaRepository<Contract, Long> {
     boolean existsByName(String name);
+    Page<Contract> findAllByOrderByNameAsc(Pageable pageable);
 }
