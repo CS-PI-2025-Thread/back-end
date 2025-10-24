60e545ff1bb39fbc8c2b74680e14d16b2f73a578
ThallesSorrilha <thallessorrilha@gmail.com>
2025-10-24 19:52:52 -0300

    feature(dto/mapper, dto/list): adicionando mapper e list de dtos para convenio

---
+++ src/main/java/com/ifpr/thread/stilofit/dto/mapper/AgreementMapper.java
@@ -0,0 +1,29 @@
+package com.ifpr.thread.stilofit.dto.mapper;
+
+import com.ifpr.thread.stilofit.dto.AgreementResponseDTO;
+import com.ifpr.thread.stilofit.dto.list.AgreementListDTO;
+import com.ifpr.thread.stilofit.models.Agreement;
+
+public class AgreementMapper {
+
+    public static AgreementResponseDTO toResponse(Agreement agreement) {
+        AgreementResponseDTO dto = new AgreementResponseDTO();
+        dto.setId(agreement.getId());
+        dto.setName(agreement.getName());
+        dto.setDescription(agreement.getDescription());
+        dto.setDiscountType(agreement.getDiscountType());
+        dto.setValue(agreement.getValue());
+        dto.setTimesApplied(agreement.getTimesApplied());
+        return dto;
+    }
+
+    public static AgreementListDTO toList(Agreement agreement) {
+        AgreementListDTO dto = new AgreementListDTO();
+        dto.setId(agreement.getId());
+        dto.setName(agreement.getName());
+        dto.setDiscountType(agreement.getDiscountType());
+        dto.setValue(agreement.getValue());
+        dto.setTimesApplied(agreement.getTimesApplied());
+        return dto;
+    }
+}
