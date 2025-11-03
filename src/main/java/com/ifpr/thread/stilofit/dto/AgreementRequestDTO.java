1b754503c5b5759aad9e389b79cc4248d75ffd92
ThallesSorrilha <thallessorrilha@gmail.com>
2025-11-03 17:07:36 -0300

    feature(models/Agreement, AgreementRequestDTO, AgreementResponseDTO): adicionando atributo de mínimo de conveniados

--- src/main/java/com/ifpr/thread/stilofit/dto/AgreementRequestDTO.java
+++ src/main/java/com/ifpr/thread/stilofit/dto/AgreementRequestDTO.java
@@ -1,10 +1,14 @@
 package com.ifpr.thread.stilofit.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class AgreementRequestDTO extends DiscountRequestDTO {
-    
+    @NotNull(message = "{validation.partnersMinimum.notnull}")
+    @Positive(message = "{validation.partnersMinimum.positive}")
+    private Integer partnersMinimum;
 }
--- src/main/java/com/ifpr/thread/stilofit/models/Agreement.java
+++ src/main/java/com/ifpr/thread/stilofit/models/Agreement.java
@@ -1,10 +1,13 @@
 package com.ifpr.thread.stilofit.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -17,5 +20,10 @@ public class Agreement extends Discount {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "partners_minimum")
+    @NotNull(message = "{validation.partnersMinimum.notnull}")
+    @Positive(message = "{validation.partnersMinimum.positive}")
+    private Integer partnersMinimum;
 
 }
